76fa4a810b59ae8ecc01235121e6b5d535fa1068
“cristopher” <curibe26@aoiths.org>
2026-04-15 14:05:06 -0400

    finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,11 @@
-const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoiY3Jpc3MxMSIsImEiOiJjbW8wYzZmd3QwNnJiMnFvbjkyc2toMHA0In0.Ce66-J1xuSKIi7eBeC7P2A";
+const MAPBOX_ACCESS_TOKEN = config.MAPBOX_API;
 mapboxgl.accessToken = "";
 
 mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
 
 const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11', 
-    center: [-74.5, 40],
-    zoom: 9
+	container: 'map',
+	style: 'mapbox://styles/mapbox/streets-v11',
+	center: [-74.5, 40],
+	zoom: 9
 });
